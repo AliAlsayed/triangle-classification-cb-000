33902e7696ea73399876e6cc51b0f4816b5af8f5
Ali Alsayed <a_alsayed87@hotmail.com>
2018-11-15 12:39:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
 class Triangle
-  # write code here
+  def initialize(a, b, c)
+    @sides = [a, b, c]
+  end
+
+  def kind
+    
+  end
+
+  def invalid?
+    @sides.any?{|x| x == 0}
+  end
+
+  class TriangleError < StandardError
+    
+  end
 end
